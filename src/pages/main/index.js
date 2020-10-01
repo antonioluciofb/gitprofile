7f9c949504e98662d1a1743a83b9cf33566e1aad
Antonio <antonioluciofb@hotmail.com>
2020-09-30 23:12:00 -0300

    .

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
-import {AiFillGithub} from "react-icons/all"
-import { Container, Header, Menu } from "./style"
+import {AiFillGithub, BsSearch} from "react-icons/all"
+import { Container, Header, Menu, ButtonUser, ButtonRepos, ButtonSearch } from "./style"
 import "./style.js";
 
 
@@ -16,16 +16,27 @@ export default class Main extends Component {
 
                 <AiFillGithub size={180} color="#FFFFFF"/>
                 <h2>Profile</h2>
-
                 </Header>
 
                 <Menu>
                     
-                    <div>
-                    <div className="user">User</div>
-                    <div className="repos">Repos</div>
-                    </div>
-                    <input></input>
+                <div className="buttons">
+
+                <ButtonUser type="button">User</ButtonUser>
+
+                <ButtonRepos type="button">Repos</ButtonRepos>
+
+                </div>
+                
+                
+                <div className="search"> 
+                <input type="text" placeholder="Nome do Usuário"></input>
+                <ButtonSearch>
+                <BsSearch size={24} color="black"/>
+                </ButtonSearch>
+                </div>
+
+                
 
                 </Menu>
 
--- src/pages/main/style.js
+++ src/pages/main/style.js
@@ -1,13 +1,13 @@
 import styled from "styled-components"
 
-export const Container = styled.div`
+export const Container = styled.div `
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
 `
 
-export const Header = styled.div`
+export const Header = styled.div `
 
     display: flex;
     margin-top: 3%;
@@ -27,38 +27,94 @@ export const Header = styled.div`
 
 `
 
-export const Menu = styled.div`
-    width: 770px;
+export const Menu = styled.div `
+    width: 40.1%;
     height: 175px;
     background-color: #FFFFFF;
     border-radius: 5px;
+    font-family: 'Dosis', sans-serif;
+    display: flex;
+    flex-direction:column;
 
-    .user {
+    .buttons{
+        box-shadow: 1px 3px 8px rgba(0,0,0,0.3);
+    }
+
+    .search{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width:100%;
+
+    }
+
+    input {
+        align-self: center;
+        margin-top: 6%;
+        text-align: center;
+        width: 30%;
+        outline: none;
+        border: 1px solid #d5d4d2;
+        padding: .625rem;
+        vertical-align: middle;
+        box-sizing: border-box;
+        box-shadow: 0px 5px 8px rgba(0,0,0,0.3);
+        transition: all .2s ease-in-out;
+    }
+
+    input:focus {
+
+        border: 2px solid #4A92FE;
+
+        ::placeholder{
+           color: transparent;
+        }
+    }
+
+`
+export const ButtonUser = styled.button`
+    cursor: pointer;
+    border:0;
+    outline:0;
+    letter-spacing: 4px;
+    width: 49.9%;
+    height: 42px;
     display:flex;
     float: left;
-    font-family: 'Dosis', sans-serif;
-    font-weight: 700;
-    font-size: 24px;
+    font-weight: 500;
+    font-size: 18px;
     align-items: center;
     justify-content: center;
     border-radius: 5px 0px 0px 0px;
     background-color: #FE4D4D;
-    width: 385px;
+    text-align: center;
+`
+
+export const ButtonRepos = styled.button `
+    cursor: all-scroll;
+    border:0;
+    outline:0;
+    letter-spacing: 4px;
+    width: 49.9%;
     height: 42px;
-
-    }
-
-    .repos {
     display:flex;
-    float: left;
-    font-family: 'Dosis', sans-serif;
-    font-weight: 700;
-    font-size: 24px;
+    float: right;
+    font-weight: 500;
+    font-size: 18px;
     align-items: center;
     justify-content: center;
-    border-radius: 0px 5px 5px 0px;
+    border-radius: 0px 5px 0px 0px;
     background-color: #4A92FE;
-    width: 385px;
-    height: 42px;
-    }
+    text-align: center;
+
+`
+
+export const ButtonSearch = styled.button`
+    border:0;
+    outline:0;
+    background-color: transparent;
+    cursor: cell;
+    margin-top: 6%;
+    margin-left: 2%;
+
 `
